4f8c3849fdf17603a8ce367e8c709cf498fc06fd
javaChd <18618110839@163.com>
2022-01-08 14:30:53 +0800

    测试3

--- Order.java
+++ Order.java
@@ -1,14 +1,8 @@
-package cn.itcast.order.pojo;
 
-import cn.itcast.feign.pojo.User;
-import lombok.Data;
-
-@Data
 public class Order {
     private Long id;
     private Long price;
     private String name;
     private Integer num;
     private Long userId;
-    private User user;
 }
